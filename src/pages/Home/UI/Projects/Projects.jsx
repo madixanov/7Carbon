280f81512d1f64eb8b92dc4213e277df800252ff
Нщлги <yoqub.madixanov@outlook.com>
2025-09-04 14:19:44 +0500

    Integrating Swiper JS

--- src/pages/Home/UI/Projects/Projects.jsx
+++ src/pages/Home/UI/Projects/Projects.jsx
@@ -1,6 +1,11 @@
-import "./Projects.css"
-import mainProject from "../../../../assets/photo/main-porject.svg"
-import project from "../../../../assets/photo/project.svg"
+import "./Projects.css";
+import mainProject from "../../../../assets/photo/main-porject.svg";
+import project from "../../../../assets/photo/project.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import { Pagination } from "swiper/modules";
 
 export default function Projects() {
   return (
@@ -11,31 +16,80 @@ export default function Projects() {
         <img src={mainProject} alt="main-project" />
       </div>
       <div className="projects-row">
-        <div className="project">
-          <img src={project} alt="project" />
-          <h3>Работа 1</h3>
-          <span>Описание текста  самой карточки.<br />Этот текст написан для визуального отображение</span>
-          <button>ПОДРОБНЕЕ</button>
-        </div>
-        <div className="project">
-          <img src={project} alt="project" />
-          <h3>Работа 1</h3>
-          <span>Описание текста  самой карточки.<br />Этот текст написан для визуального отображение</span>
-          <button>ПОДРОБНЕЕ</button>
-        </div>
-        <div className="project">
-          <img src={project} alt="project" />
-          <h3>Работа 1</h3>
-          <span>Описание текста  самой карточки.<br />Этот текст написан для визуального отображение</span>
-          <button>ПОДРОБНЕЕ</button>
-        </div>
-        <div className="project">
-          <img src={project} alt="project" />
-          <h3>Работа 1</h3>
-          <span>Описание текста  самой карточки.<br />Этот текст написан для визуального отображение</span>
-          <button>ПОДРОБНЕЕ</button>
-        </div>
+        <Swiper
+          spaceBetween={30}
+          pagination={{ clickable: true }}
+          modules={[Pagination]}
+          className="mySwiper"
+          breakpoints={{
+            320: {
+              slidesPerView: 2, // телефоны побольше
+            },
+            768: {
+              slidesPerView: 2, // планшеты
+            },
+            1024: {
+              slidesPerView: 3, // десктоп
+            },
+          }}
+        >
+          <SwiperSlide>
+            <div className="project">
+              <img src={project} alt="project" />
+              <h3>Работа 1</h3>
+              <span>
+                Описание текста самой карточки.
+                <br />Этот текст написан для визуального отображения
+              </span>
+              <button>ПОДРОБНЕЕ</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="project">
+              <img src={project} alt="project" />
+              <h3>Работа 2</h3>
+              <span>
+                Описание текста самой карточки.
+                <br />Этот текст написан для визуального отображения
+              </span>
+              <button>ПОДРОБНЕЕ</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="project">
+              <img src={project} alt="project" />
+              <h3>Работа 3</h3>
+              <span>
+                Описание текста самой карточки.
+                <br />Этот текст написан для визуального отображения
+              </span>
+              <button>ПОДРОБНЕЕ</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="project">
+              <img src={project} alt="project" />
+              <h3>Работа 4</h3>
+              <span>
+                Описание текста самой карточки.
+                <br />Этот текст написан для визуального отображения
+              </span>
+              <button>ПОДРОБНЕЕ</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="project">
+              <img src={project} alt="project" />
+              <h3>Работа 5</h3>
+              <span>
+                Описание текста самой карточки.
+                <br />Этот текст написан для визуального отображения
+              </span>
+              <button>ПОДРОБНЕЕ</button>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
-  )
+  );
 }
